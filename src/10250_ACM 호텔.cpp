a95e59a80737b02062ecef9ecf6b50ca37aa1d07
MakerJun46 <guswns6711@naver.com>
2021-12-26 22:33:41 +0900

    12211226

---
+++ src/10250_ACM 호텔.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int T;
+	int H, W, N;
+
+	cin >> T;
+
+	for (int t = 0; t < T; t++)
+	{
+		cin >> H >> W >> N;
+
+		int h = (N % H == 0 ? H : N % H);
+		int w = (N % H == 0 ? N / H : N / H + 1);
+
+		if (w < 10)
+			cout << h << "0" << w << endl;
+		else
+			cout << h << w << endl;
+	}
+
+	return 0;
+}
