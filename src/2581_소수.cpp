73e832202b03536e8a1abb353729ccb92146b631
MakerJun46 <guswns6711@naver.com>
2022-01-02 23:33:30 +0900

    20220102

---
+++ src/2581_소수.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int M, N;
+
+	cin >> M >> N;
+
+	int minPrime = 10000;
+	int sumPrime = 0;
+
+	for (int i = M; i <= N; i++)
+	{
+		if (i == 1)
+			continue;
+
+		bool isPrime = true;
+
+		for (int j = 2; j < i; j++)
+		{
+			if (i % j == 0)
+			{
+				isPrime = false;
+				break;
+			}
+		}
+
+		if (isPrime)
+		{
+			sumPrime += i;
+
+			if (minPrime > i)
+				minPrime = i;
+		}
+	}
+	
+	if (sumPrime == 0)
+		cout << -1;
+	else
+		cout << sumPrime << endl << minPrime;
+
+	return 0;
+}
