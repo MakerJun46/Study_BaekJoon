e4d0ca1de0a4924b0b4cd8060d81050ed9ce43bf
MakerJun46 <guswns6711@naver.com>
2022-01-07 17:40:52 +0900

    20220107

---
+++ src/2231_분해합.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int N;
+	int constructor = 0;
+
+	cin >> N;
+
+	for (int i = 1; i < N; i++)
+	{
+		int M = i;
+		int sum = i;
+
+		while (M > 0)
+		{
+			sum += M % 10;
+			M /= 10;
+		}
+
+		if (sum == N)
+		{
+			constructor = i;
+			break;
+		}
+	}
+
+	cout << constructor;
+
+	return 0;
+}
