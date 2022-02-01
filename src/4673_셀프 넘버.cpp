87045706514a2a594b1da706824a69f27601221f
MakerJun46 <guswns6711@naver.com>
2022-02-01 21:37:45 +0900

    220201

---
+++ src/4673_셀프 넘버.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	vector<bool> numbers(10001, false);
+
+	for (int i = 1; i < 10001; i++)
+	{
+		int tmp = i;
+		int self_number = i;
+		while (tmp > 0)
+		{
+			self_number += tmp % 10;
+			tmp /= 10;
+		}
+
+		if(self_number < 10001)
+			numbers[self_number] = true;
+	}
+
+	for (int i = 1; i < 10001; i++)
+	{
+		if (!numbers[i])
+			cout << i << "\n";
+	}
+
+	return 0;
+}
